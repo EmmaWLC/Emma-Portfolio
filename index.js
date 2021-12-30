06387ed081f16da087dc74fcba91589c12fc9f3d
EmmaWLC <weilin.emma.chen@gmail.com>
2021-12-30 13:28:56 +0800

    first version of scroll effect

--- index.js
+++ index.js
@@ -17,22 +17,61 @@
     tl.to('.title-3 .letter', {opacity: 1, y: '0', clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',stagger: '.03'}, "-=.7")
     tl.from('p', {opacity: 0, y: '-50px'}, "-=.7")
 
+//snap effect
+    ScrollTrigger.defaults({
+        toggleActions: "restart none none none",
+        scroller: ".scroll-to-next-part"
+      });
 //work scroll effect
-gsap.registerPlugin(ScrollTrigger);
-const wt =document.querySelector('.work-section-title');
-gsap.to(wt, {
+
+let tl2 = gsap.timeline({
     scrollTrigger: {
-        trigger:wt,
-        toggleActions: "play none none none"
-    },
-    x: 400,
-    duration: 1
+        trigger: ".section2-0",
+        toggleActions: "restart pause restart pause",
+        start: "top center", 
+        markers: true
+      }
 });
 
+tl2.from('.work-section-title1',{ x: -200, opacity: 0, duration: 1.5 })
+    .from('.work-detail1',{ x: -200, opacity: 0, duration: 1 },"<+=.5")
+    .from('.work-title1',{ x: 200, opacity: 0, duration: 1 },"-=1")
+    .from('.work-img1',{ x: 200, opacity: 0, duration: 1.5 },"<+=.3")
+
+
+let tl3 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".section2-1",
+        toggleActions: "restart pause restart pause",
+        start: "top center", 
+        markers: true
+      }
+});
+
+tl3.from('.work-section-title2',{ x: -200, opacity: 0, duration: 1.5 })
+    .from('.work-detail2',{ x: -200, opacity: 0, duration: 1 },"<+=.5")
+    .from('.work-title2',{ x: 200, opacity: 0, duration: 1 },"-=1")
+    .from('.work-img2',{ x: 200, opacity: 0, duration: 1.5 },"<+=.3")
+
+let tl4 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".section2-2",
+        toggleActions: "restart pause restart pause",
+        start: "top center", 
+        markers: true
+      }
+});
+
+tl4.from('.work-section-title3',{ x: -200, opacity: 0, duration: 1.5 })
+    .from('.work-detail3',{ x: -200, opacity: 0, duration: 1 },"<+=.5")
+    .from('.work-title3',{ x: 200, opacity: 0, duration: 1 },"-=1")
+    .from('.work-img3',{ x: 200, opacity: 0, duration: 1.5 },"<+=.3")
+
+   
 
 //contact title mouse effect
     let contactTitle = document.querySelector('.contact-title');
-    let cursorCircle = document.querySelector('.cursor');
+    // let cursorCircle = document.querySelector('.cursor');
 
     let cursor = {
         x: 0,
